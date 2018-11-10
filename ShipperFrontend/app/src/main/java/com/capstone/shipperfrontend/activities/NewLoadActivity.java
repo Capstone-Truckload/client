26e71fe4ff532a487406f3710ed5c82911742135
jbarber0 <jbarber0@users.noreply.github.com>
2018-11-10 10:28:13 -0600

    newload, accounts

--- ShipperFrontend/app/src/main/java/com/capstone/shipperfrontend/activities/NewLoadActivity.java
+++ ShipperFrontend/app/src/main/java/com/capstone/shipperfrontend/activities/NewLoadActivity.java
@@ -5,11 +5,33 @@ import android.os.Bundle;
 
 import com.capstone.shipperfrontend.R;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.capstone.shipperfrontend.R;
+import com.capstone.shipperfrontend.models.HashedPassword;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class NewLoadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_load);
+    }
+
+    protected void onClick(View view){
+        EditText loadName = findViewById(R.id.loadName);
+        EditText origin = findViewById(R.id.originAddress);
+        EditText destination = findViewById(R.id.destinationAddress);
+
     }
 }
